1e2bcb002d63459e99914b802ec3b6029b0f0351
upadhuma <umang.upadhayay@adidas.com>
2025-03-04 23:47:05 +0530

    repl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
 
         for(int i=0;i<10;i++){
+            System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             System.out.println(input+": command not found");
