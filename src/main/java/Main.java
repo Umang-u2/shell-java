c963b5e92af7aae838969d045d3c4f37fbf0ad1f
upadhuma <umang.upadhayay@adidas.com>
2025-03-05 00:26:16 +0530

    type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,14 +12,26 @@ public class Main {
             String input = scanner.nextLine();
             String command = input.split("\\s+")[0];
             int firstSpaceIndex = input.indexOf(" ");
+            String arguments = (firstSpaceIndex == -1)?"":input.substring(firstSpaceIndex+1);
+            List<String> acceptedCommands = getShellCommands();
             if(command.equalsIgnoreCase("exit")){
                 return;
             } else if(command.equalsIgnoreCase("echo")){
-                System.out.println((firstSpaceIndex == -1)?"":input.substring(firstSpaceIndex+1));
+                System.out.println(arguments);
+            } else if(command.equalsIgnoreCase("type") && acceptedCommands.contains(arguments)){
+                System.out.println(arguments+" is a shell builtin");
             }
             else {
                 System.out.println(input + ": command not found");
             }
         }
+    }
+
+    private static List getShellCommands() {
+        List<String> shellCommands = new ArrayList<String>();
+        shellCommands.add("echo");
+        shellCommands.add("exit");
+        shellCommands.add("type");
+        return shellCommands;
     }
 }
